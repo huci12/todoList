739137d529ffd5d94d03e5f72b4381929a77b1cb
burns.choi <huci12@naver.com>
2019-01-09 00:29:50 +0900

    Initial commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>TODO it!</div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    h1{
+        color : #2F3B52;
+        font-weight: 900;
+        margin : 2.5rem 0 1.5rem;
+    }
+
+</style>
